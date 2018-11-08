a73ef2b12d841aaa01ddcba229803b8743a72792
Artizazel <43548024+Artizazel@users.noreply.github.com>
2018-11-08 15:47:40 +0000

    Add files via upload

---
+++ Loops and list boxes/Loops and list boxes/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Loops_and_list_boxes
+{
+    partial class Numbers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstNumbers = new System.Windows.Forms.ListBox();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.textMax = new System.Windows.Forms.TextBox();
+            this.txtMin = new System.Windows.Forms.TextBox();
+            this.lblMax = new System.Windows.Forms.Label();
+            this.lblMin = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstNumbers
+            // 
+            this.lstNumbers.FormattingEnabled = true;
+            this.lstNumbers.Location = new System.Drawing.Point(297, 42);
+            this.lstNumbers.Name = "lstNumbers";
+            this.lstNumbers.Size = new System.Drawing.Size(264, 342);
+            this.lstNumbers.TabIndex = 0;
+            // 
+            // btnUp
+            // 
+            this.btnUp.Location = new System.Drawing.Point(55, 193);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(169, 82);
+            this.btnUp.TabIndex = 1;
+            this.btnUp.Text = "Up";
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
+            // 
+            // btnDown
+            // 
+            this.btnDown.Location = new System.Drawing.Point(55, 302);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(169, 82);
+            this.btnDown.TabIndex = 2;
+            this.btnDown.Text = "Down";
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+            // 
+            // textMax
+            // 
+            this.textMax.Location = new System.Drawing.Point(140, 61);
+            this.textMax.Name = "textMax";
+            this.textMax.Size = new System.Drawing.Size(83, 20);
+            this.textMax.TabIndex = 3;
+            this.textMax.TextChanged += new System.EventHandler(this.textMax_TextChanged);
+            // 
+            // txtMin
+            // 
+            this.txtMin.Location = new System.Drawing.Point(141, 127);
+            this.txtMin.Name = "txtMin";
+            this.txtMin.Size = new System.Drawing.Size(83, 20);
+            this.txtMin.TabIndex = 4;
+            this.txtMin.TextChanged += new System.EventHandler(this.txtMin_TextChanged);
+            // 
+            // lblMax
+            // 
+            this.lblMax.AutoSize = true;
+            this.lblMax.Location = new System.Drawing.Point(74, 64);
+            this.lblMax.Name = "lblMax";
+            this.lblMax.Size = new System.Drawing.Size(27, 13);
+            this.lblMax.TabIndex = 5;
+            this.lblMax.Text = "Max";
+            this.lblMax.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblMin
+            // 
+            this.lblMin.AutoSize = true;
+            this.lblMin.Location = new System.Drawing.Point(74, 130);
+            this.lblMin.Name = "lblMin";
+            this.lblMin.Size = new System.Drawing.Size(24, 13);
+            this.lblMin.TabIndex = 6;
+            this.lblMin.Text = "Min";
+            // 
+            // Numbers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblMin);
+            this.Controls.Add(this.lblMax);
+            this.Controls.Add(this.txtMin);
+            this.Controls.Add(this.textMax);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnUp);
+            this.Controls.Add(this.lstNumbers);
+            this.Name = "Numbers";
+            this.Text = "Loops and listboxes";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstNumbers;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.TextBox textMax;
+        private System.Windows.Forms.TextBox txtMin;
+        private System.Windows.Forms.Label lblMax;
+        private System.Windows.Forms.Label lblMin;
+    }
+}
+
---
+++ Loops and list boxes/Loops and list boxes/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Loops_and_list_boxes
+{
+    public partial class Numbers : Form
+    {
+        public Numbers()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerateNumbers(string minString, string maxString)
+        {
+            try
+            {
+                int min = Convert.ToInt32(minString);
+                int max = Convert.ToInt32(maxString);
+
+                for (int i = min; i <= max; i++)
+                {
+                    lstNumbers.Items.Add("Number " + i);
+                }
+            }
+            catch
+            {
+                
+            }
+        }
+
+        private void GenerateNumbersBack(string minString, string maxString)
+        {
+            try
+            {
+                int min = Convert.ToInt32(minString);
+                int max = Convert.ToInt32(maxString);
+
+                for (int i = min; i >= max; i--)
+                {
+                    lstNumbers.Items.Add("Number " + i);
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textMax_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtMin_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUp_Click(object sender, EventArgs e)
+        {
+            GenerateNumbers(txtMin.Text, textMax.Text);
+        }
+
+        private void btnDown_Click(object sender, EventArgs e)
+        {
+            GenerateNumbersBack(textMax.Text, txtMin.Text);
+        }
+    }
+}
