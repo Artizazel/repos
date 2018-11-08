6da089980b8fecd575ac679da5f8a9c10532b317
Artizazel <43548024+Artizazel@users.noreply.github.com>
2018-11-08 16:55:02 +0000

    Add files via upload

---
+++ Arrays/Arrays/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Arrays
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstNumbers = new System.Windows.Forms.ListBox();
+            this.txtRows = new System.Windows.Forms.TextBox();
+            this.txtCols = new System.Windows.Forms.TextBox();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstNumbers
+            // 
+            this.lstNumbers.FormattingEnabled = true;
+            this.lstNumbers.Location = new System.Drawing.Point(11, 83);
+            this.lstNumbers.Name = "lstNumbers";
+            this.lstNumbers.Size = new System.Drawing.Size(777, 355);
+            this.lstNumbers.TabIndex = 0;
+            this.lstNumbers.SelectedIndexChanged += new System.EventHandler(this.lstNumbers_SelectedIndexChanged);
+            // 
+            // txtRows
+            // 
+            this.txtRows.Location = new System.Drawing.Point(86, 30);
+            this.txtRows.Name = "txtRows";
+            this.txtRows.Size = new System.Drawing.Size(98, 20);
+            this.txtRows.TabIndex = 1;
+            this.txtRows.TextChanged += new System.EventHandler(this.txtRows_TextChanged);
+            // 
+            // txtCols
+            // 
+            this.txtCols.Location = new System.Drawing.Point(266, 30);
+            this.txtCols.Name = "txtCols";
+            this.txtCols.Size = new System.Drawing.Size(98, 20);
+            this.txtCols.TabIndex = 2;
+            this.txtCols.TextChanged += new System.EventHandler(this.txtCols_TextChanged);
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(409, 17);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(113, 45);
+            this.btnShow.TabIndex = 3;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Rows:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(210, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Columns:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.txtCols);
+            this.Controls.Add(this.txtRows);
+            this.Controls.Add(this.lstNumbers);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstNumbers;
+        private System.Windows.Forms.TextBox txtRows;
+        private System.Windows.Forms.TextBox txtCols;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Arrays/Arrays/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arrays
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void GenerateArray(string rowsString, string colsString)
+        {
+            int rows = Convert.ToInt32(rowsString);
+            int cols = Convert.ToInt32(colsString);
+            int grid = rows * cols;
+            string output = "";
+
+            int[,] numArray = new int[rows, cols];
+
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    numArray[i, j] = (i+1) * (j+1);
+                }
+            }
+
+
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    output = output + numArray[i, j];
+                }
+                lstNumbers.Items.Add(output);
+            }
+
+
+
+
+        }
+
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            
+            GenerateArray(txtRows.Text, txtCols.Text);
+
+            
+        }
+
+        private void txtRows_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCols_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstNumbers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
